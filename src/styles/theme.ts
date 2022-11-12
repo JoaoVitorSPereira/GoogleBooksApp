6d27063631a5d27db315cd5b002c331dcea84cac
Joao <joaovitorspereira@gmail.com>
2022-11-11 22:45:08 -0300

    Feature: Styling, theme and theme types added

---
+++ src/styles/theme.ts
@@ -0,0 +1,17 @@
+import { RFValue } from 'react-native-responsive-fontsize';
+
+export default {
+  colors: {
+    primary: '#000',
+    text: '#FFF',
+    red: '#F00',
+  },
+  fonts: {
+    regular: 'Roboto-Regular',
+    bold: 'Roboto-Bold',
+    thin: 'Roboto-Thin',
+  },
+  responsive: {
+    value: RFValue,
+  },
+};
